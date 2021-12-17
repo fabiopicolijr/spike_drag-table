c541f77ec255d04b880373234eeebb0944d32490
Fábio Picoli <fabiopicolijr@gmail.com>
2021-12-17 09:54:44 -0300

    schedule initial
    This schedule was transported from DCC project.

--- src/js/example5.js
+++ src/js/example5.js
@@ -1,39 +1,33 @@
-const slider = document.querySelector('.wrapper-schedule');
-const content = document.querySelector('.schedule');
-let mouseDown = false;
-let startX, scrollLeft;
+const slider = $('.wrapper-schedule');
+const sliderContent = $('.schedule');
 
-let startDragging = function (e) {
-  mouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-  
-};
-let stopDragging = function (event) {
-  mouseDown = false;
+slider['mouseDown'] = false;
+slider['startX'] = 0;
 
-  content.style.cursor = "grab"
-};
-
-slider.addEventListener('mousemove', (e) => {
-  e.preventDefault();
-  if(!mouseDown) { return; }
-  const x = e.pageX - slider.offsetLeft;
-  const scroll = x - startX;
-  slider.scrollLeft = scrollLeft - scroll;
-
-  content.style.cursor = "grabbing"
-});
-
-// Add the event listeners
-slider.addEventListener('mousedown', startDragging, false);
-slider.addEventListener('mouseup', stopDragging, false);
-slider.addEventListener('mouseleave', stopDragging, false);
-
-
-/* zoeira */
-function blink_text() {
-  $('.blink').fadeOut(500);
-  $('.blink').fadeIn(500);
+function handleStartDragging(event){
+  slider.mouseDown = true;  
+  slider.startX = event.pageX - slider.offset().left;  
+  sliderContent.css('cursor', 'grabbing');
 }
-setInterval(blink_text, 1000);
+
+function handleStopDragging() { 
+  slider.mouseDown = false;
+  sliderContent.css('cursor', 'grab'); 
+}
+
+function handleDragging(event) {  
+  event.preventDefault();
+
+  if(!slider.mouseDown) return;
+
+  const x = event.pageX - slider.offset().left;
+  const scroll = x - slider.startX;
+
+  slider.scrollLeft(slider.scrollLeft() - scroll);
+  sliderContent.css('cursor', 'grabbing'); 
+}
+
+slider.on('mousedown', handleStartDragging);
+slider.on('mouseup', handleStopDragging);
+slider.on('mouseleave', handleStopDragging);
+slider.on('mousemove', handleDragging);
